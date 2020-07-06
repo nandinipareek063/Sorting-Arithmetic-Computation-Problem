8108be9581616cd0f13d15189b4d0af4f2d3b697
nandinipareek063 <nandinipareek063@gmail.com>
2020-07-06 19:38:49 +0530

    Added: Take three inputs is added

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,1 +1,8 @@
-Welcome to Sorting Arithmetic Computation Problem
+echo "Welcome to Sorting Arithmetic Computation Problem"
+
+#UC1
+echo "Write program totake three inputs–a, b & c"
+read -p "Enter a:" a
+read -p "Enter b:" b
+read -p "Enter c:" c
+
